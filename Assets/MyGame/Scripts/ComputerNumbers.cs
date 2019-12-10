bc940f73fd269ea5ae9054ac0fa6ad04d84846ee
sarahglavan <sarah.glava@htl-salzburg.ac.at>
2019-12-10 17:11:02 +0100

    final commit all changes refactoring

---
+++ Assets/MyGame/Scripts/ComputerNumbers.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputerNumbers : MonoBehaviour
+{
+    public InputField input1;
+    public InputField input2;
+    public Text result;
+
+    private float varA;
+    private float varB;
+
+    public void SetResult()
+    {
+        result.text = SubstractNumbers();
+    }
+
+    private string SubstractNumbers()
+    {
+        varA = float.Parse(input1.text);
+        varB = float.Parse(input2.text);
+
+        string success = (varA - varB).ToString();
+        return success;
+    }
+
+    public void ResetCounter()
+    {
+        input1.text = "";
+        input2.text = "";
+        result.text = "";
+    }
+}
